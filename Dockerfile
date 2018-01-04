79d172efb688a80d65dab09e33e38abe287c1416
egee-irl <brian@egee.io>
2018-01-03 23:28:25 -0500

    selinux damn you

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM egeeio/minecraftserver-modded
 COPY cfg/eula.txt /opt/minecraft/eula.txt
-COPY run.sh /opt/minecraft/run.sh
+# COPY run.sh /opt/minecraft/run.sh
 CMD ["/opt/minecraft/run.sh"]
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xms4G -Xmx4G -jar /opt/minecraft/forge-1.10.2-12.18.3.2511-universal.jar --noconsole
+java -Xms4G -Xmx4G -jar /opt/minecraft/forge-1.10.2-12.18.3.2511-universal.jar nogui
